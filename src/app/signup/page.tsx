530dca714ce7bd767f38de7aa730897fd25ecffd
Mikhail Saburov <saburov1985@gmail.com>
2024-05-19 22:49:31 +1100

    верстка страницы регистрации

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+
 export default function SignupPage() {
-  return <div>SignupPage</div>;
+  const [user, setUser] = useState({
+    email: '',
+    username: '',
+    password: '',
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Войти</h1>
+    </div>
+  );
 }
