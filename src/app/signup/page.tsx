68d74a65f77d4e3d72c972a78b155e789042cad1
Mikhail Saburov <saburov1985@gmail.com>
2024-05-20 22:42:58 +1100

    верстка формы регистрации

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,40 +16,41 @@ export default function SignupPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-      <h1 className="font-semibold text-2xl">Зарегестрируйся</h1>
-      <hr />
-      <br />
-      <label htmlFor="username">Имя пользователя</label>
-      <input
-        className="p-2 rounded-md"
-        type="text"
-        name="username"
-        value={user.username}
-        onChange={(e) => setUser({ ...user, username: e.target.value })}
-        placeholder="введите имя пользователя"
-      />
-      <hr />
-      <br />
-      <label htmlFor=" email">Адрес эл. почты</label>
-      <input
-        className="p-2 rounded-md"
-        type="text"
-        name=" email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        placeholder="example@example.com"
-      />
-      <hr />
-      <br />
-      <label htmlFor="password">Веедите пароль</label>
-      <input
-        className="p-2 rounded-md"
-        type="text"
-        name="password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder="придумай пароль"
-      />
+      <h1 className="font-semibold text-2xl">Регистрация</h1>
+      <div className="flex flex-col">
+        <br />
+        <label htmlFor="username">Имя пользователя</label>
+        <input
+          className="p-2 rounded-md"
+          type="text"
+          name="username"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          placeholder="введите имя пользователя"
+        />
+
+        <br />
+        <label htmlFor=" email">Адрес эл. почты</label>
+        <input
+          className="p-2 rounded-md"
+          type="email"
+          name=" email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="example@example.com"
+        />
+
+        <br />
+        <label htmlFor="password">Веедите пароль</label>
+        <input
+          className="p-2 rounded-md"
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="придумай пароль"
+        />
+      </div>
     </div>
   );
 }
