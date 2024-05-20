316495418d6b3c52b0bfa01676a754ef8f359b18
Mikhail Saburov <saburov1985@gmail.com>
2024-05-20 22:27:05 +1100

    добавил бордер

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,8 +15,10 @@ export default function SignupPage() {
   const onSignup = async () => {};
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Войти</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
+      <div className="border border-red-600 p-5 rounded-lg">
+        <h1>Войти</h1>
+      </div>
     </div>
   );
 }
