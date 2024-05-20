019b474d51bdafc1accad3af61433a15d43b2c39
Mikhail Saburov <saburov1985@gmail.com>
2024-05-20 22:39:20 +1100

    добавить форму регистрации

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,9 +16,40 @@ export default function SignupPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-      <div className="border border-red-600 p-5 rounded-lg">
-        <h1>Зарегестрируйся</h1>
-      </div>
+      <h1 className="font-semibold text-2xl">Зарегестрируйся</h1>
+      <hr />
+      <br />
+      <label htmlFor="username">Имя пользователя</label>
+      <input
+        className="p-2 rounded-md"
+        type="text"
+        name="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="введите имя пользователя"
+      />
+      <hr />
+      <br />
+      <label htmlFor=" email">Адрес эл. почты</label>
+      <input
+        className="p-2 rounded-md"
+        type="text"
+        name=" email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="example@example.com"
+      />
+      <hr />
+      <br />
+      <label htmlFor="password">Веедите пароль</label>
+      <input
+        className="p-2 rounded-md"
+        type="text"
+        name="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="придумай пароль"
+      />
     </div>
   );
 }
