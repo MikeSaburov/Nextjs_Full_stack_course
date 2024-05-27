329b9de12e1caee376562c77edd72a42cbc04f9d
Mikhail Saburov <saburov1985@gmail.com>
2024-05-27 22:29:00 +1100

    создать api регистрации пользователя (написал логику)

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from '@/dbConfig/dbConfig';
 import User from '@/models/userModel';
 import { NextRequest, NextResponse } from 'next/server';
 import bcryptjs from 'bcryptjs';
+import { log } from 'console';
 
 connect();
 
@@ -9,6 +10,37 @@ export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
     const { username, email, password } = reqBody;
+
+    console.log(reqBody);
+
+    //проверка существует ли пользователь
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.j('Пользователь с таким email уже существует', {
+        status: 400,
+      });
+    }
+
+    //хеширование пароля
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    //создание пользователя
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+
+    console.log(savedUser);
+
+    return NextResponse.json({
+      message: 'Пользователь создан успешно!',
+      success: true,
+      savedUser,
+    });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
