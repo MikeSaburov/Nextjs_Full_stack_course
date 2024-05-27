fc1794bfac7c806b86cecf1c680c1b25121481a4
Mikhail Saburov <saburov1985@gmail.com>
2024-05-27 21:48:15 +1100

    создать модель пользователя

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,3 +1,35 @@
+import { verify } from 'crypto';
 import mongoose from 'mongoose';
+import { type } from 'os';
 
-const userSchema = await mongoose.Schema({});
+const userSchema = await mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Пожадуйста введите имя пользователя'],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Пожадуйста введите email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Пожадуйста введите пароль'],
+  },
+  isVerfied: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPaaswordToken: String,
+  forgotPaaswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const User = mongoose.models.users || mongoose.model('User', userSchema);
+export default User;
