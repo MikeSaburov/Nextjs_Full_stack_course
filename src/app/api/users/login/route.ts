783f41ab524375f6c12595a397c79149a200231f
Mikhail Saburov <saburov1985@gmail.com>
2024-05-30 22:50:34 +1100

    Написать логику для входа

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,1 +1,34 @@
+import { connect } from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
 
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    //проверяем есть ли пользователь в системе
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: 'Пользователь не существует' },
+        { status: 400 }
+      );
+    }
+
+    //проверяем правильность пароля
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { error: 'Неправильный пароль' },
+        { status: 400 }
+      );
+    }
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
