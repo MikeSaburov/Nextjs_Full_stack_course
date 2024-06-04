dc1a58db1470ea719ef91da8c5d4a5be23a6d5af
Mikhail Saburov <saburov1985@gmail.com>
2024-06-04 21:16:14 +1100

    Написать логику входа в систему

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,52 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function LoginPage() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
   const [user, setUser] = useState({
     email: '',
     username: '',
     password: '',
   });
 
-  const onLogin = async () => {};
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/auth/login', user);
+      console.log('Вход успешен', response.data);
+      toast.success('Вы вошли в систему!');
+      setTimeout(() => {
+        router.push('/profile');
+      }, 3000);
+    } catch (error: any) {
+      console.log('Неверный логин', error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
       <h1 className="font-semibold text-2xl">Войти</h1>
       <div className="flex flex-col">
+        <Toaster position="top-left" reverseOrder={false} />
         {/* <br />
         <label htmlFor="username">Имя пользователя</label>
         <input
