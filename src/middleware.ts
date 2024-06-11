febfd8874761c736c649bbe5a5d24f8f926fb5ef
Mikhail Saburov <saburov1985@gmail.com>
2024-06-11 21:10:02 +1100

    написать логику проверки вошел ли пользователь в систему, если да, то закрыт для него страницы входп и регистрации

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,13 @@ import type { NextRequest } from 'next/server'
  
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  const path = request.nextUrl.pathname
+  const isPublicPath = path==='/login' || path==='/signup'
+  const token = request.cookies.get('token')?.value || ''
+
+  if(isPublicPath && token){
+    return NextResponse.redirect(new URL('/', request.url))
+  }
 }
  
 
