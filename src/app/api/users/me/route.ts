9f15257f62fd304be39da5032aa5c83c60ebeeff
Mikhail Saburov <saburov1985@gmail.com>
2024-06-11 22:50:32 +1100

    получить данные пользователя

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,15 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import {connect} from '@/dbConfig/dbConfig'
+connect()
+
+export  async  function  GET(req: NextRequest) {
+try {
+   const userId = await getDataFromToken(req)
+   const user = await User.findOne({_id:userId}).select('-password')
+   return NextResponse.json({message: 'Пользователь найден', data: user})
+} catch (error:any) {
+    return NextResponse.json({error:error.message}, {status:400})
+}
+}
