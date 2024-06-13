86778ee318268161e4628fc9bf13392e1ddb521d
Mikhail Saburov <saburov1985@gmail.com>
2024-06-13 21:35:08 +1100

    создать логику  маршрута для подвтерждения

--- src/app/api/users/veryfyemail/route.ts
+++ src/app/api/users/veryfyemail/route.ts
@@ -1,14 +1,37 @@
 import { connect } from "@/dbConfig/dbConfig";
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
 
-connect()
-
+connect();
 
 export async function POST(req: NextRequest) {
-    try {
-        
-    } catch (error:any) {
-        return NextResponse.json({message:error.message})
+  try {
+    const reqBody = await req.json();
+    const { token } = reqBody;
+    console.log(token);
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Недействительный токен" },
+        { status: 401 }
+      );
     }
+    console.log(user);
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      { message: "Пользователь успешно активирован", success: true },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
