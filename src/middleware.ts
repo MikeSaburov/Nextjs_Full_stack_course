143c2d94b1747494d035c7526868b1ad8b562867
Mikhail Saburov <saburov1985@gmail.com>
2024-06-18 22:09:32 +1100

    добавить путь в middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,22 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname
+  const path = request.nextUrl.pathname;
 
-  
-  const isPublicPath = path === '/login' || path === '/signup'
- 
-  const token = request.cookies.get('token')?.value || ''
+  const isPublicPath = path === "/login" || path === "/signup";
 
-  if(isPublicPath && token){
-    return NextResponse.redirect(new URL('/', request.nextUrl))
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
-  if(!isPublicPath && !token){
-    return NextResponse.redirect(new URL('/login', request.nextUrl))
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
 }
- 
 
 export const config = {
-  matcher: [
-    '/',
-    '/login',
-    '/profile',
-    '/signup',
-  ],
-}
+  matcher: ["/", "/login", "/profile", "/signup", "/verifyemail"],
+};
